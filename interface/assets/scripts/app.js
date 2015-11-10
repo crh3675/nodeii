74b4bf9f4e9fa7cc0c1b43ed0bb3ca1ff1c9c960
Craig Hoover <crh3675@gmail.com>
2015-11-09 20:43:27 -0500

    adding comments

--- interface/assets/scripts/app.js
+++ interface/assets/scripts/app.js
@@ -1,3 +1,12 @@
+/*
+ * We don't care to compile your stuff.  Turn on Gzip in your web-server.
+ * If you really want to build a pre-compile nightmare, you can install one of these
+ * and DIY
+ * - http://gruntjs.com/
+ * - http://bower.io/
+ * - https://github.com/webpack/webpack
+ */
+
 App = window.App = {};
 
 (function(){
--- interface/routes/main.js
+++ interface/routes/main.js
@@ -1,4 +1,9 @@
-module.exports = function(req, res, buf) {
+module.exports = function(req, res, next) {
+   
+   /*
+    * Read the Waterline documentation for ORM methods
+    * https://github.com/balderdashy/waterline
+    */
    
    Student.create({ name : 'Kid' } ).exec(function(err) {
       
@@ -6,7 +11,17 @@ module.exports = function(req, res, buf) {
          console.error(err);
       }
       
+      console.log('Created student');
+      
       Student.findOne({name: 'Kid'}).exec(function(err, result) {
+         
+         if(err) {
+            console.error(err);
+         }
+         
+         if(result) {
+            console.log('Found student');   
+         }
          
          var isOpen = Registration.isOpen();
          
