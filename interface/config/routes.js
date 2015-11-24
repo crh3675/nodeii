1eaf83ddedce7d3bfcbcb38a738979b7dc6d17f8
Craig Hoover <crh3675@gmail.com>
2015-11-24 18:55:07 -0500

    comment update to config routes

--- interface/config/routes.js
+++ interface/config/routes.js
@@ -1,6 +1,8 @@
 /**
  Routing uses the ExpressJS routing framework conventions
-
+ 
+ Policies are our custom solution to `before_filter` if you want to 
+ inject authentication validation or any other pre-rendering logic like i18n
 */
 module.exports = {
    'get *'       : {
